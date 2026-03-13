f7891d37ab5a1479c10c352541fc84525b5a51ad
Hexin <hexium310@gmail.com>
2026-03-14 03:33:04 +0900

    Remove deprecated vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { cloudflare } from "@cloudflare/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths(),
     cloudflare(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
   css: {
     modules: {
       localsConvention: "camelCaseOnly",
